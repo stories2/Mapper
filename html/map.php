20dccc0ef6fce29d8225d883ce03cc283916e863
HYUNWOO KIM <stories282@gmail.com>
2020-12-06 23:35:29 +0900

    select and return json

--- html/map.php
+++ html/map.php
@@ -3,19 +3,33 @@
 
     $mysqli = new mysqli($DB_SERVER, $DB_USER, $DB_PASSWORD, $DB_DB_NAME);
 
-    if (!$mysqli->multi_query('CALL API_TO_WEB_SELECT_MAP_POLYGON_USING_DISTANCE(37.5542103, 126.9870975, 5, 0, 100)')) {
-        echo "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
-    }
-
-    do {
-        if ($res = $mysqli->store_result()) {
-            printf("---\n");
-            var_dump($res->fetch_all());
-            $res->free();
-        } else {
-            if ($mysqli->errno) {
-                echo "Store failed: (" . $mysqli->errno . ") " . $mysqli->error;
-            }
+    $stmt = $mysqli->prepare('CALL API_TO_WEB_SELECT_MAP_POLYGON_USING_DISTANCE(37.513224, 127.1001228, 5, 0, 100)');
+//    $stmt->execute(array(':param1' => $param1, ':param2' => $param2));
+    $stmt->execute();
+//    if (!$mysqli->multi_query('CALL API_TO_WEB_SELECT_MAP_POLYGON_USING_DISTANCE(37.513224, 127.1001228, 5, 0, 100)')) {
+//        echo "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
+//    }
+//https://stackoverflow.com/a/1686047/7270469
+    try {
+        $result = array();
+        $abc = $stmt->get_result();
+//        print_r($abc);
+        $cnt = 0;
+        $row = $abc->fetch_assoc();
+        if ($row) {
+            $cnt = $row['cnt'];
         }
-    } while ($mysqli->more_results() && $mysqli->next_result());
+        while($row = $abc->fetch_assoc()) {
+//            var_dump($row);
+            $result[] = array('EMD_CD' => $row['EMD_CD'], 'EMD_ENG_NM' => $row['EMD_ENG_NM'],
+                'EMD_KOR_NM' => $row['EMD_KOR_NM'], 'type' => $row['type'],
+                'center_lat'=> $row['center_lat'], 'center_lot' => $row['center_lot'],
+            'distance' => $row['distance']
+            , 'coordinates' => json_decode($row['coordinates']));
+        }
+        header('Content-type: application/json');
+        echo json_encode( array('result' => 'OK', 'data'=> array('cnt'=>$cnt, 'data'=>$result)) );
+    } catch(Exception $e) {
+        echo json_encode(array('result' => 'FAIL', 'message' => $e->getMessage()));
+    }
 ?>
--- html/store.php
+++ html/store.php
@@ -3,19 +3,32 @@ require_once 'global.php';
 
 $mysqli = new mysqli($DB_SERVER, $DB_USER, $DB_PASSWORD, $DB_DB_NAME);
 
-if (!$mysqli->multi_query('CALL API_TO_WEB_SELECT_STORE_USING_DISTANCE(37.513224, 127.1001228, 5, 0, 100)')) {
-    echo "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
-}
-
-do {
-    if ($res = $mysqli->store_result()) {
-        printf("---\n");
-        var_dump($res->fetch_all());
-        $res->free();
-    } else {
-        if ($mysqli->errno) {
-            echo "Store failed: (" . $mysqli->errno . ") " . $mysqli->error;
-        }
+$stmt = $mysqli->prepare('CALL API_TO_WEB_SELECT_STORE_USING_DISTANCE(37.513224, 127.1001228, 5, 0, 100)');
+//    $stmt->execute(array(':param1' => $param1, ':param2' => $param2));
+$stmt->execute();
+//    if (!$mysqli->multi_query('CALL API_TO_WEB_SELECT_MAP_POLYGON_USING_DISTANCE(37.513224, 127.1001228, 5, 0, 100)')) {
+//        echo "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
+//    }
+//https://stackoverflow.com/a/1686047/7270469
+try {
+    $result = array();
+    $abc = $stmt->get_result();
+//        print_r($abc);
+    $cnt = 0;
+    $row = $abc->fetch_assoc();
+    if ($row) {
+        $cnt = $row['cnt'];
     }
-} while ($mysqli->more_results() && $mysqli->next_result());
+    while($row = $abc->fetch_assoc()) {
+//            var_dump($row);
+        $result[] = array('s_code' => $row['s_code'], 's_name' => $row['s_name'],
+            'tel' => $row['tel'], 'fax' => $row['fax'],
+            'addr'=> $row['addr'], 'doro_address' => $row['doro_address'], 'lat' => $row['lat'],
+            'lot'=>$row['lot'], 'distance'=>$row['distance']);
+    }
+    header('Content-type: application/json');
+    echo json_encode( array('result' => 'OK', 'data'=> array('cnt'=>$cnt, 'data'=>$result)) );
+} catch(Exception $e) {
+    echo json_encode(array('result' => 'FAIL', 'message' => $e->getMessage()));
+}
 ?>
