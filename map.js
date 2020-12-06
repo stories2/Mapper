4ec91518571ee05a39e71d88223955ae631bbe91
HYUNWOO KIM <stories282@gmail.com>
2020-12-06 21:15:48 +0900

    map polygon

--- map.js
+++ map.js
@@ -30,4 +30,16 @@ console.log('0 point', result[0])
 const fs = require('fs');
 fs.writeFileSync('map.json', JSON.stringify(result));
 
-const sql = `insert into `
+let sql = `insert into Map_Polygon(EMD_CD, EMD_ENG_NM, EMD_KOR_NM, \`type\`, center_lat, center_lot, coordinates) VALUES \
+(${result[0].EMD_CD}, \'${result[0].EMD_ENG_NM}\', \'${result[0].EMD_KOR_NM}\', \'${result[0].type}\', \'${result[0].center.lat}\', \'${result[0].center.lot}\', \
+\'${JSON.stringify(result[0].coordinates)}\')`
+
+const values = result.map(i => {
+    return `insert into Map_Polygon(EMD_CD, EMD_ENG_NM, EMD_KOR_NM, \`type\`, center_lat, center_lot, coordinates) VALUES (${i.EMD_CD}, \'${i.EMD_ENG_NM}\', \'${i.EMD_KOR_NM}\', \'${i.type}\', ${i.center.lat}, ${i.center.lot}, \'${JSON.stringify(i.coordinates)}\')`
+})
+
+sql = values.join(';\n');
+
+fs.writeFileSync('map.sql', sql);
+
+console.log('len', values.length, values[values.length - 1]);
